409ed10038aa87f4277b4933fd8e7699014b07c9
Mr-Explorer142 <tahmidalamtamim142@gmail.com>
2025-09-21 23:42:51 +0600

    Add navbar, Outlet and Link to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
+import { Children, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
 import "./index.css";
 import App from "./App.jsx";
+import Navbar from "./layout/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import About from "./pages/About.jsx";
@@ -14,23 +15,29 @@ import NotFound from "./pages/NotFound.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/products",
-    element: <Products></Products>,
-  },
-    {
-    path: "/about",
-    element: <About></About>,
-  },
-    {
-    path: "/contact",
-    element: <Contact></Contact>,
-  },
-    {
-    path: "*",
-    element: <NotFound></NotFound>,
+    element: <Navbar></Navbar>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "*",
+        element: <NotFound></NotFound>,
+      },
+    ],
   },
 ]);
 
